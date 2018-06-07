a6c1216e13e51089c9e335a74cc41306d3625acf
Apassara Jurapan <ajurapan@sfu.ca>
2018-06-06 18:32:38 -0700

    slingshot can be pulled by controller by trigger

--- BobaBattleVR/Assets/Slingshot.cs
+++ BobaBattleVR/Assets/Slingshot.cs
@@ -8,22 +8,59 @@ public class Slingshot : MonoBehaviour {
 
     public GameObject slingGO; //slingGameObject
 
+    private Vector3 slingShotStart; //use to remember the start position
+
     private GameObject currBall;
     private bool ready = true; //tell us whether the slingshot is loaded or not
 
+    private SteamVR_TrackedObject trackedController;
+    private bool inSlingShot = false;
+
 	// Use this for initialization
 	void Start () {
-		
+        slingShotStart = slingGO.transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(ready)
+
+    // Update is called once per frame
+    void Update() {
+        if (ready)
         {
             currBall = Instantiate(ballPrefab);
             currBall.transform.parent = slingGO.transform;
             currBall.transform.localPosition = Vector3.zero;
             ready = false; // make it false here to make it don't keep sprawning a ball
         }
+
+        if (trackedController != null)
+        {
+            var device = SteamVR_Controller.Input((int)trackedController.index);
+            if (inSlingShot)
+            {
+                if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+                {
+                    slingGO.transform.position = trackedController.transform.position;
+                }
+            }
+        }
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        trackedController = other.GetComponent<SteamVR_TrackedObject>();
+        if(trackedController != null)
+        {
+            inSlingShot = true;
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        trackedController = other.GetComponent<SteamVR_TrackedObject>();
+        if (trackedController != null)
+        {
+            inSlingShot = false;
+        }
+
+    }
 }
