1a7f6195db54c75d0f520de19741c1e38ff3de91
Apassara Jurapan <ajurapan@sfu.ca>
2018-06-13 22:35:04 -0700

    pick up and drop the ball

---
+++ BobaBattleVR/Assets/Scripts/PickUpParent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+public class PickUpParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate () {
+      
+	}
+
+    void OnTriggerStay(Collider col)
+    {
+        GetComponent<Slingshot>();
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger) && col.name =="Ball(Clone)")
+        {
+            Debug.Log("col.name" + col.name);
+            //pick up a ball, the ball transform is now with the controller
+            Debug.Log("You have collided with " + col.name + " while holding down touch");
+            col.attachedRigidbody.isKinematic = true; //makethe ridgid body not affected by the physic system
+            col.gameObject.transform.SetParent(gameObject.transform);
+            Slingshot.ballHolding = true;
+
+            // ***** Now need to make it load to the bandbone and stay there********
+            //mulitiple on triggr stay, if the object is this blah blah??
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger) && col.name == "Ball(Clone)")
+        {
+            //drop the ball 
+            Debug.Log("object released");
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+            col.attachedRigidbody.useGravity = true;
+            Slingshot.ballHolding = false;
+
+        }
+    }
+}
---
+++ BobaBattleVR/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Slingshot : MonoBehaviour {
+
+   
+
+    public GameObject ballPrefab;
+    public GameObject slingGO; //slingGameObject
+    public GameObject ballStand;
+
+    private GameObject currBall;
+    private Vector3 slingShotStart; //use to remember the start position
+
+    public static bool ballHolding = false;
+    private bool inSlingShot = false;
+    private bool ready = true; //spawn a ball when true
+     
+
+    void  Start() {
+        slingShotStart = slingGO.transform.position;
+	}
+
+   
+    void FixedUpdate() {
+        Debug.Log("In fixed update");
+
+        if (ready)
+        {
+            Debug.Log("In SlingShot.cs ready");
+            currBall = Instantiate(ballPrefab);
+            currBall.transform.parent = ballStand.transform;
+            currBall.transform.localPosition = Vector3.zero; 
+            ready = false; // make it false here to make it don't keep sprawning a ball
+        }
+
+            //if (!inSlingShot) //when the ball is not in Slingshot
+            //{
+            //    if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+            //    {
+                    
+            //    }
+
+            //    //when the ball touch the slingshot (collider) OnTriggerEnter? 
+            //    //change the ball transform on the slingshot (coded), inSlingShot = true, shot the ball(coded)
+            //}
+	}
+
+  
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    trackedController = other.GetComponent<SteamVR_TrackedObject>();
+    //    if (trackedController != null)
+    //    {
+    //        inSlingShot = true;
+    //    }
+    //}
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    trackedController = other.GetComponent<SteamVR_TrackedObject>();
+    //    if (trackedController != null)
+    //    {
+    //        inSlingShot = false;
+    //    }
+    //}
+}
